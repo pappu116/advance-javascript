7bc51efc327cb9bd386251dbb3a42b22d1217f06
pappu116 <pappublak@gmail.com>
2020-08-06 22:36:57 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//Falsy:
+// 0
+//False
+//""
+// undefined
+//null
+// NaN
+//Truthy
+// '0', ' ', [], {}
+
+let name= false;
+if(name){
+    console.log("this is truly value")
+}else{
+    console.log("this is falsy value")
+}
