dc3c072056aa498472c35d20bfc523d5b903db21
pappu116 <pappublak@gmail.com>
2020-08-06 23:16:02 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,19 @@
+const first = 0;
+
+const second = false;
+
+if(first == second){
+    console.log("this is true value")
+}else{
+    console.log("this is false value")   // double equal defined to only valo convert property value and nearest rejult some match this show rejult its true
+}
+
+const first = 0;
+
+const second = false;
+
+if(first ===second){
+    console.log("this is true value")   //=== defined to value or type and rejult show 
+}else{
+    console.log("this is false value")
+}
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,2 +1,20 @@
+//undefined finding way
+
 let pakhi:
-Console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1+ num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+
+const premik={name:"khuli", phone:2514685};
+console.log(premik.gf);
+
+let fun = undefined; // this is rejurved word
+console.log(fun);
+
